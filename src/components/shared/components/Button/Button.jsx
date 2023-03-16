aa738acf767796085b6a4c581912c10c971b644b
Makiiev Oleksandr <alsermak@gmail.com>
2023-03-16 10:43:37 +0200

    delete useless variables

--- src/components/shared/components/Button/Button.jsx
+++ src/components/shared/components/Button/Button.jsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import css from './button.module.scss';
 
 const MyButton = ({ children, size, type = 'submit', onClick, ...props }) => {
   return (
